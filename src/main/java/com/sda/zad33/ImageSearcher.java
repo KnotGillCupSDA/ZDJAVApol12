86167ec91a7fce20dbc9c4ea1830896c704fb0f4
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2020-08-08 10:49:36 +0200

    - zad 33 - directory traversal: Files.walk, File.walkFileTree

--- src/main/java/com/sda/zad33/ImageSearcher.java
+++ src/main/java/com/sda/zad33/ImageSearcher.java
@@ -1,24 +1,58 @@
 package com.sda.zad33;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ImageSearcher {
 
     public List<File> getImages(Path inputDirectory) {
-        List<File> images = new ArrayList<>();
         File inputFile = inputDirectory.toFile();
         if (inputFile.isDirectory()) {
-            for (File file : inputFile.listFiles((dir, name) -> {
+            File[] filtered = inputFile.listFiles((dir, name) -> {
                 String lowerCase = name.toLowerCase();
                 return lowerCase.endsWith(".png") || lowerCase.endsWith("jpg");
-            })) {
-                images.add(file);
+            });
+
+            if (filtered != null) {
+                return Arrays.asList(filtered);
             }
         }
+        return Collections.emptyList();
+    }
 
-        return images;
+    public List<File> getImagesWithWalk(Path inputDirectory) throws IOException {
+        try (Stream<Path> walk = Files.walk(inputDirectory, 1)) {
+            return walk
+                .map(Path::toFile)
+                .filter(file -> file.getName().endsWith(".png") || file.getName().endsWith(".jpg"))
+                .collect(Collectors.toList());
+        }
+    }
+
+    public List<File> getImagesWithWalkTree(Path inputDirectory) throws IOException {
+        List<File> files = new ArrayList<>();
+        Files.walkFileTree(inputDirectory, Collections.emptySet(), 1, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                File f = file.toFile();
+                if (f.getName().endsWith(".jpg") || f.getName().endsWith(".png")) {
+                    files.add(f);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        return files;
     }
 }
--- src/main/java/com/sda/zad33/Main.java
+++ src/main/java/com/sda/zad33/Main.java
@@ -1,14 +1,17 @@
 package com.sda.zad33;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ImageSearcher imageSearcher = new ImageSearcher();
 
-        System.out.println(
-            imageSearcher.getImages(
-                Paths.get("src", "main", "resources", "zad33")));
+        Path inputDirectory = Paths.get("src", "main", "resources", "zad33");
+        System.out.println(imageSearcher.getImages(inputDirectory));
+        System.out.println(imageSearcher.getImagesWithWalk(inputDirectory));
+        System.out.println(imageSearcher.getImagesWithWalkTree(inputDirectory));
 
     }
 }
